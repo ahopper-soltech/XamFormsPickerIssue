6b6882279f736468674ca497b58ed77e76e938d5
unknown-user <unknown-user@10.0.0.220>
2014-09-10 14:39:00 -0400

    Initial check-in of module PickerIssue

---
+++ PickerIssue/Home.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace PickerIssue
+{	
+	public partial class Home : ContentPage
+	{	
+		public Home ()
+		{
+      System.Diagnostics.Debug.WriteLine("Home::ctor() - Enter");
+			InitializeComponent ();
+      System.Diagnostics.Debug.WriteLine("Home::ctor() - Exit");
+		}
+  }
+}
+
